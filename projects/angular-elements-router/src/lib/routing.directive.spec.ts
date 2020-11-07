30c900593e94e7b03e98dffe334a1e3c143603bc
fboeller <fabianboeller@googlemail.com>
2020-11-07 12:38:52 +0100

    Add routing directive

---
+++ projects/angular-elements-router/src/lib/routing.directive.spec.ts
@@ -0,0 +1,41 @@
+import { Router } from '@angular/router';
+import { RoutingDirective } from './routing.directive';
+
+describe('RoutingDirective', () => {
+  let directive: RoutingDirective;
+  let navigateByUrlSpy: jasmine.Spy;
+  let navigateSpy: jasmine.Spy;
+
+  beforeEach(() => {
+    const router: Partial<Router> = {
+      navigateByUrl: () => Promise.resolve(true),
+      navigate: () => Promise.resolve(true)
+    };
+    const activatedRoute = {
+      parent: 'parent'
+    };
+    navigateByUrlSpy = spyOn(router, 'navigateByUrl');
+    navigateSpy = spyOn(router, 'navigate');
+    directive = new RoutingDirective(null as any, router as any, activatedRoute as any);
+  });
+
+  it('navigates relative to the base href if the route is prefixed with /root', () => {
+    directive.navigateToUrl('/root/my-external-route');
+    expect(navigateByUrlSpy).toBeCalledWith('/my-external-route');
+  });
+
+  it('navigates to the root when called with /root', () => {
+    directive.navigateToUrl('/root');
+    expect(navigateByUrlSpy).toBeCalledWith('');
+  });
+
+  it('navigates relative to the activated route if the route is not prefixed with /root', () => {
+    directive.navigateToUrl('/a/b');
+    expect(navigateSpy).toBeCalledWith(['./a/b'], { relativeTo: 'parent' });
+  });
+
+  it('navigates to the activated route itself if the route is /', () => {
+    directive.navigateToUrl('/');
+    expect(navigateSpy).toBeCalledWith(['./'], { relativeTo: 'parent' });
+  });
+});
---
+++ projects/angular-elements-router/src/lib/routing.directive.ts
@@ -0,0 +1,53 @@
+import { Directive, ElementRef, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
+import { Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+
+const urlSegmentsToString = (segments: UrlSegment[]) => '/' + segments.map((segment) => segment.path).join('/');
+
+/**
+ * Enables the routing feature on a custom element.
+ * It passes the activated route into the custom element and listens to route changes of the custom element.
+ * The custom element needs to define an input `route` and an output `routeChange`.
+ *
+ * ```
+ * @Component({
+ *   selector: 'my-custom-element-host',
+ *   template: `
+ *     <my-custom-element aerRouting></lx-custom-element>
+ *   `
+ * })
+ * export class MyCustomElementHostComponent {}
+ * ```
+ */
+@Directive({
+  selector: '[aerRouting]'
+})
+export class RoutingDirective implements OnInit, OnDestroy {
+  private destroyed$ = new Subject<void>();
+
+  constructor(private element: ElementRef, private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.url.pipe(map(urlSegmentsToString), takeUntil(this.destroyed$)).subscribe((url) => (this.element.nativeElement.route = url));
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+  }
+
+  @HostListener('routeChange', ['$event'])
+  handleRouteChange(event: { detail?: string }) {
+    this.navigateToUrl(event?.detail);
+  }
+
+  navigateToUrl(url: string | undefined) {
+    if (url && url.startsWith('/root')) {
+      this.router.navigateByUrl(url.substring('/root'.length));
+    } else if (url && url.startsWith('/')) {
+      this.router.navigate(['./' + url.substring('/'.length)], { relativeTo: this.route.parent });
+    } else {
+      console.warn(`The aerRouting retrieved a route change that does not start with a '/'.`);
+    }
+  }
+}

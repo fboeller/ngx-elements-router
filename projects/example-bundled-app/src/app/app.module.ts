207cc1d410f0c17dfc090dd30cf3e99cb8bac336
fboeller <fabianboeller@googlemail.com>
2020-11-07 16:00:07 +0100

    Create example bundled app

---
+++ projects/example-bundled-app/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { Injector, NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { EntryComponent } from './entry.component';
+import { createCustomElement } from '@angular/elements';
+
+@NgModule({
+  declarations: [EntryComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [EntryComponent],
+})
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const customElement = createCustomElement(EntryComponent, {
+      injector: this.injector,
+    });
+    window.customElements.define('aer-entry', customElement);
+  }
+}

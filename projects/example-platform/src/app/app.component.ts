7c19e5c6c4cd97e757429e08633e1343584d4f82
fboeller <fabianboeller@googlemail.com>
2020-11-08 11:25:57 +0100

    Add links to example platform

--- projects/example-platform/src/app/app.component.ts
+++ projects/example-platform/src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>I'm in the platform</h1>
+    <a routerLink="/micro-frontend/child">Go to child page</a>
+    <a routerLink="/micro-frontend">Go to micro frontend main page</a>
+    <a routerLink="/platform-page">Go to platform page</a>
     <router-outlet></router-outlet>
   `,
 })
